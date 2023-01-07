d11cd981994f7f0b774c62be964274a0631ef1dc
Victor Colvin <trackstar171@yahoo.com>
2023-01-07 18:44:28 -0500

    clear

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//Your Code here
+window.prompt(
+    'You are about to embark on a journey or either left or right decisions. How will you choose? Choose carefully! Click Ok to proceed...')
+    let firstAnswer  = window.prompt('Do you head left or right?')
+    console.log(firstAnswer)
+if(firstAnswer === 'left')
+{ let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+(secondAnswer === 'follow')
+    let 
+}
+
+else if(firstAnswer === 'right') 
+{
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
+
